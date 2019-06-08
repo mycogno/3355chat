b198b82583cd0e10b4bbf38f2e38e725cae5b4dd
Unknown <jdg4661@gmail.com>
2019-06-08 23:36:06 +0900

    fix

--- 서버/mysite/mysite/settings.py
+++ 서버/mysite/mysite/settings.py
@@ -75,12 +75,17 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# mysite/settings.py
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'TEST': {
+            'NAME': os.path.join(BASE_DIR, 'db_test.sqlite3')
+        }
     }
 }
+
 
 
 # Password validation
